ab64e3bb08be358bda274a52e46af71e4bacfd65
deem65 <davidsoloca05@gmail.com>
2026-08-25 19:27:48 +0300

    capture and pixel extraction

--- 715clipper/715clipper.cpp
+++ 715clipper/715clipper.cpp
@@ -1,6 +1,8 @@
 #include <Windows.h>
-#include <iostream>
 #include "715clipper.h"
+
+#include <iostream>
+#include <vector>
 
 int main()
 {
@@ -27,14 +29,90 @@ int main()
 
 void OnClipHotkeyPressed()
 {
-    HDC ScreenDC = GetDC(nullptr);
+    HDC screenDc = GetDC(nullptr);
 
-    if (ScreenDC == nullptr) {
+    if (screenDc == nullptr) {
         std::cerr << "failed to access screen\n";
         return;
     }
 
     std::cout << "screen accessed\n";
 
-    ReleaseDC(nullptr, ScreenDC);
+    HDC memoryDc = CreateCompatibleDC(screenDc);
+
+    if (memoryDc == nullptr) {
+        std::cerr << "failed to create memory dc\n";
+        ReleaseDC(nullptr, screenDc);
+        return;
+    }
+
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    HBITMAP screenBitmap = CreateCompatibleBitmap(
+        screenDc,
+        screenWidth,
+        screenHeight
+    );
+
+    if (screenBitmap == nullptr) {
+        std::cerr << "failed to create screen bitmap\n";
+
+        DeleteDC(memoryDc);
+        ReleaseDC(nullptr, screenDc);
+        return;
+    }
+
+    std::cout 
+        << "screen size: "
+        << screenWidth
+        << "x"
+        << screenHeight
+        << '\n';
+
+    HGDIOBJ previousSelectedObject = SelectObject(memoryDc, screenBitmap); 
+
+    if (previousSelectedObject == nullptr) {
+        std::cerr << "failed to select bitmap into memory dc\n";
+
+        DeleteObject(screenBitmap);
+        DeleteDC(memoryDc);
+        ReleaseDC(nullptr, screenDc);
+        return;
+    }
+
+    if (!BitBlt(memoryDc, 0, 0, screenWidth, screenHeight, screenDc, 0, 0, SRCCOPY))
+    {
+        std::cerr << "failed to copy screen pixels\n";
+    }
+    else
+    {
+        std::cout << "screen captured into memory\n";
+    }
+
+    BITMAPINFO bitmapInfo{};
+
+    bitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bitmapInfo.bmiHeader.biWidth = screenWidth;
+    bitmapInfo.bmiHeader.biHeight = -screenHeight;
+    bitmapInfo.bmiHeader.biPlanes = 1;
+    bitmapInfo.bmiHeader.biBitCount = 32;
+    bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    std::vector<unsigned char> pixelBytes(screenWidth * screenHeight * 4); 
+
+    SelectObject(memoryDc, previousSelectedObject); //GetDIBits() expects the bitmap not to be selected into a dc
+
+    int copiedScanLines = GetDIBits(screenDc, screenBitmap, 0, screenHeight, pixelBytes.data(), &bitmapInfo, DIB_RGB_COLORS); //Win32 C style API which requires pointers
+
+    if (copiedScanLines == screenHeight) {
+        std::cout << "extraction succeeded";
+    }
+    else {
+        std::cerr << "extraction failed";
+;    }
+
+    DeleteObject(screenBitmap);
+    DeleteDC(memoryDc);
+    ReleaseDC(nullptr, screenDc);
 }
