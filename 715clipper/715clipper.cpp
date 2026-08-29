51b659b0f6cd087c67a85f3c03b60d57ab0f0c71
deem65 <davidsoloca05@gmail.com>
2026-08-29 14:54:32 +0300

    refactor frame capture

--- 715clipper/715clipper.cpp
+++ 715clipper/715clipper.cpp
@@ -5,134 +5,205 @@
 #include <climits>
 #include <vector>
 #include <fstream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <atomic>
+#include <optional>
+#include <utility>
+
+std::atomic<bool> captureInProgress{ false };
 
 int main()
 {
-    constexpr int ClipHotkeyId = 1;
+    constexpr int clipHotkeyId = 1;
 
-    if (!RegisterHotKey(nullptr, ClipHotkeyId, MOD_CONTROL | MOD_SHIFT, VK_F7)) {
+    HWND window = nullptr; //hardcoded for now
+
+    if (!RegisterHotKey(nullptr, clipHotkeyId, MOD_CONTROL | MOD_SHIFT, VK_F7)) {
         std::cerr << "cliphotkey registry: fail\n";
         return 1;
     }
-    std::cout << "cliphotkey registry: success\n";
 
     std::cout << "running\n";
 
     MSG message{};
 
     while (GetMessage(&message, nullptr, 0, 0) > 0) {
-        if (message.message == WM_HOTKEY && message.wParam == ClipHotkeyId) {
-            OnClipHotkeyPressed();
+        if (message.message == WM_HOTKEY && message.wParam == clipHotkeyId) {
+            on_clip(window);
         }
     }
 
-    UnregisterHotKey(nullptr, ClipHotkeyId);
+    UnregisterHotKey(nullptr, clipHotkeyId);
 
     return 0;
 }
+void on_clip(HWND window) {
+    bool prev = captureInProgress.exchange(true); 
 
-void OnClipHotkeyPressed()
+    if (prev) {
+        std::cerr << "capture already in progress\n";
+        return;
+    }
+
+    std::thread capture([window]() {
+        capture_multi_frames(window);
+        captureInProgress.store(false);
+        });
+
+    capture.detach();
+}
+bool initialize_frame_context(FrameContext& context, HWND window) {
+    context.window = window;
+
+    if (!get_window_dc(context.windowDc, context.window)) {
+        return false;
+    }
+
+    if (!get_memory_dc(context.memoryDc, context.windowDc)) {
+        ReleaseDC(context.window, context.windowDc);
+        return false;
+    }
+
+    if (!get_window_dimensions(context.window, context.width, context.height)){
+        DeleteDC(context.memoryDc);
+        ReleaseDC(context.window, context.windowDc);
+        return false;
+    }
+
+    if (!get_window_bitmap(context.windowBitmap, context.windowDc, context.width, context.height))
+    {
+        DeleteDC(context.memoryDc);
+        ReleaseDC(context.window, context.windowDc);
+        return false;
+    }
+
+    return true;
+}
+
+void capture_multi_frames(HWND window)
+{
+    constexpr int targetFPS = 5;
+    constexpr int durationSeconds = 2;
+    constexpr int frameCount = targetFPS * durationSeconds;
+    constexpr int intervalNs = 1'000'000'000 / targetFPS;
+
+    auto interval = std::chrono::nanoseconds(intervalNs);    
+    auto nextTimePoint = std::chrono::steady_clock::now();
+
+    FrameContext context{};
+
+    if (!initialize_frame_context(context, window)) {
+        std::cerr << "frame context initialization: fail\n";
+        return;
+    }
+
+    std::vector<Frame> frames;
+    std::optional<Frame> frame;
+
+    frames.reserve(frameCount);
+
+    for (int i = 0; i < frameCount; i++) {
+        frame = capture_frame(context);
+
+        if (frame.has_value()) {
+            frames.push_back(std::move(frame.value()));
+        }
+
+        if (i < frameCount - 1) {
+            nextTimePoint += interval;
+            std::this_thread::sleep_until(nextTimePoint);
+        }
+    }
+
+    cleanup_frame_context(context);
+
+    for (int i = 0; i < frames.size(); i++) {
+        save_bitmap(frames[i].bitmapHeader, frames[i].pixelBytes, i);
+    }
+}
+bool get_window_dc(HDC& windowDc, HWND window) {
+    windowDc = GetDC(window);
+    return windowDc != nullptr;
+}
+bool get_memory_dc(HDC& memoryDc, HDC screenDc) {
+    memoryDc = CreateCompatibleDC(screenDc);
+    return memoryDc != nullptr;
+}
+bool get_window_dimensions(HWND window, int& width, int& height) {
+    if (window == nullptr) {
+        //multi monitor selection will be added later
+        width = GetSystemMetrics(SM_CXSCREEN);
+        height = GetSystemMetrics(SM_CYSCREEN);
+        return true;
+    }
+
+    RECT rect{};
+
+    if (!GetClientRect(window, &rect)) {
+        return false;
+    }
+
+    width = rect.right - rect.left;
+    height = rect.bottom - rect.top;
+
+    return width > 0 && height > 0;
+}
+bool get_window_bitmap(HBITMAP& windowBitmap, HDC windowDc, int width, int height) {
+    windowBitmap = CreateCompatibleBitmap(windowDc, width, height);
+    return windowBitmap != nullptr;
+}
+void cleanup_frame_context(FrameContext& context)
+{
+    if (context.windowBitmap != nullptr) {
+        DeleteObject(context.windowBitmap);
+    }
+    if (context.memoryDc != nullptr) {
+        DeleteDC(context.memoryDc);
+    }
+    if (context.windowDc != nullptr) {
+        ReleaseDC(context.window, context.windowDc);
+    }
+}
+std::optional<Frame> capture_frame(FrameContext& context)
 {
     constexpr int bitsPerPixel = 32;
 
-    HDC screenDc = GetDC(nullptr);
-
-    if (screenDc == nullptr) {
-        std::cerr << "screen dc: fail\n";
-        return;
-    }
-
-    std::cout << "screen dc: success\n";
-
-    HDC memoryDc = CreateCompatibleDC(screenDc);
-
-    if (memoryDc == nullptr) {
-        std::cerr << "memory dc: fail\n";
-        ReleaseDC(nullptr, screenDc);
-        return;
-    }
-
-    std::cout << "memory dc: success\n";
-
-    //temporarily gets screen dimensions of primary screen
-    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
-
-    HBITMAP screenBitmap = CreateCompatibleBitmap(
-        screenDc,
-        screenWidth,
-        screenHeight
-    );
-
-    if (screenBitmap == nullptr) {
-        std::cerr << "screen bitmap: fail\n";
-
-        DeleteDC(memoryDc);
-        ReleaseDC(nullptr, screenDc);
-        return;
-    }
-
-    std::cout << "screen bitmap: success\n";
-
-    HGDIOBJ previousSelectedObject = SelectObject(memoryDc, screenBitmap); 
+    HGDIOBJ previousSelectedObject = SelectObject(context.memoryDc, context.windowBitmap); 
 
     if (previousSelectedObject == nullptr) {
-        std::cerr << "bitmap -> memory dc: fail\n";
-
-        DeleteObject(screenBitmap);
-        DeleteDC(memoryDc);
-        ReleaseDC(nullptr, screenDc);
-        return;
+        return std::nullopt;
     }
 
-    std::cout << "bitmap -> memory dc: success\n";
-
-    if (!BitBlt(memoryDc, 0, 0, screenWidth, screenHeight, screenDc, 0, 0, SRCCOPY))
+    if (!BitBlt(context.memoryDc, 0, 0, context.width, context.height, context.windowDc, 0, 0, SRCCOPY))
     {
-        std::cerr << "bit block transfer: faii\n";
-
-        SelectObject(memoryDc, previousSelectedObject);
-        DeleteObject(screenBitmap);
-        DeleteDC(memoryDc);
-        ReleaseDC(nullptr, screenDc);
-        return;
+        SelectObject(context.memoryDc, previousSelectedObject);
+        return std::nullopt;
     }
 
-    std::cout << "bit block transfer: success\n";
+    BITMAPINFO bitmapInfo = create_bitmap_info(context.width, context.height, bitsPerPixel);
 
-    BITMAPINFO bitmapInfo{};
+    SelectObject(context.memoryDc, previousSelectedObject); //GetDIBits expects the bitmap to be unselected in a dc
 
-    bitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-    bitmapInfo.bmiHeader.biWidth = screenWidth;
-    bitmapInfo.bmiHeader.biHeight = -screenHeight;
-    bitmapInfo.bmiHeader.biPlanes = 1;
-    bitmapInfo.bmiHeader.biBitCount = bitsPerPixel;
-    bitmapInfo.bmiHeader.biCompression = BI_RGB;
+    Frame frame{};
 
-    std::vector<unsigned char> pixelBytes(screenWidth * screenHeight * bitsPerPixel / CHAR_BIT); 
+    frame.bitmapHeader = bitmapInfo.bmiHeader;
+    frame.pixelBytes.resize(context.width * context.height * bitsPerPixel / CHAR_BIT);
 
-    SelectObject(memoryDc, previousSelectedObject); //GetDIBits expects the bitmap to be unselected in a dc
+    int copiedScanLines = GetDIBits(context.windowDc, context.windowBitmap, 0, context.height, frame.pixelBytes.data(), &bitmapInfo, DIB_RGB_COLORS);
 
-    int copiedScanLines = GetDIBits(screenDc, screenBitmap, 0, screenHeight, pixelBytes.data(), &bitmapInfo, DIB_RGB_COLORS); 
-
-    if (copiedScanLines == screenHeight) {
-        std::cout << "pixel extraction: success\n";
-        SaveBitmap(bitmapInfo.bmiHeader, pixelBytes);
+    if (copiedScanLines != context.height) {
+        return std::nullopt;
     }
-    else {
-        std::cerr << "pixel extraction: fail\n";
-        DeleteObject(screenBitmap);
-        DeleteDC(memoryDc);
-        ReleaseDC(nullptr, screenDc);
-        return;
-        }
 
-    DeleteObject(screenBitmap);
-    DeleteDC(memoryDc);
-    ReleaseDC(nullptr, screenDc);
+    return frame;
 }
-void SaveBitmap(const BITMAPINFOHEADER& bitmapHeader, const std::vector<unsigned char>& pixelBytes) { 
-    std::ofstream file("capture.bmp", std::ios::binary);
+void save_bitmap(const BITMAPINFOHEADER& bitmapHeader, const std::vector<unsigned char>& pixelBytes, int frameNum) {
+
+    std::string fileName = "_" + std::to_string(frameNum) + ".bmp";
+
+    std::ofstream file(fileName, std::ios::binary);
 
     if (!file) {
         std::cerr << "bitmap file: fail\n";
@@ -143,7 +214,7 @@ void SaveBitmap(const BITMAPINFOHEADER& bitmapHeader, const std::vector<unsigned
 
     BITMAPFILEHEADER fileHeader{};
 
-    fileHeader.bfType = 0x4D42; //win bitmap
+    fileHeader.bfType = 0x4D42; //windows bitmap
     fileHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
     fileHeader.bfSize = fileHeader.bfOffBits + static_cast<DWORD>(pixelBytes.size());
 
@@ -163,4 +234,40 @@ void SaveBitmap(const BITMAPINFOHEADER& bitmapHeader, const std::vector<unsigned
     }
 
     std::cout << "bitmap write: success\n";
+}
+BITMAPINFO create_bitmap_info(int screenWidth, int screenHeight, int bitsPerPixel) {
+    BITMAPINFO bitmapInfo{};
+
+    bitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bitmapInfo.bmiHeader.biWidth = screenWidth;
+    bitmapInfo.bmiHeader.biHeight = -screenHeight;
+    bitmapInfo.bmiHeader.biPlanes = 1;
+    bitmapInfo.bmiHeader.biBitCount = bitsPerPixel;
+    bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    return bitmapInfo;
+}
+std::optional<Frame> extract_frame_from_bitmap(HDC screenDc, HBITMAP screenBitmap,int screenWidth, int screenHeight, int bitsPerPixel)
+{
+    BITMAPINFO bitmapInfo = create_bitmap_info(screenWidth,screenHeight,bitsPerPixel);
+
+    Frame frame{};
+    frame.bitmapHeader = bitmapInfo.bmiHeader;
+    frame.pixelBytes.resize(screenWidth * screenHeight * bitsPerPixel / CHAR_BIT);
+
+    int copiedScanLines = GetDIBits(
+        screenDc,
+        screenBitmap,
+        0,
+        screenHeight,
+        frame.pixelBytes.data(),
+        &bitmapInfo,
+        DIB_RGB_COLORS
+    );
+
+    if (copiedScanLines != screenHeight) {
+        return std::nullopt;
+    }
+
+    return frame;
 }
--- 715clipper/715clipper.h
+++ 715clipper/715clipper.h
@@ -2,10 +2,46 @@
 
 #include <Windows.h>
 #include <vector>
+#include <optional>
 
-void OnClipHotkeyPressed();
+struct Frame {
+    BITMAPINFOHEADER bitmapHeader{};
+    std::vector<unsigned char> pixelBytes;
+};
 
-void SaveBitmap(
+struct FrameContext {
+    HWND window{};
+    HDC windowDc{};
+    HDC memoryDc{};
+    HBITMAP windowBitmap{};
+    BITMAPINFO bitmapInfo{};
+
+    int width{};
+    int height{};
+};
+
+void on_clip(HWND window);
+
+void capture_multi_frames(HWND window);
+
+void save_bitmap(
     const BITMAPINFOHEADER& bitmapHeader,
-    const std::vector<unsigned char>& pixelBytes
+    const std::vector<unsigned char>& pixelBytes,
+    int frameNum
 );
+
+bool get_window_dc(HDC& windowDc, HWND window);
+bool get_memory_dc(HDC& memoryDc, HDC screenDc);
+bool get_window_dimensions(HWND window, int& width, int& height);
+bool get_window_bitmap(HBITMAP& windowBitmap, HDC windowDc, int width, int height);
+bool initialize_frame_context(FrameContext& context, HWND window);
+void cleanup_frame_context(FrameContext& context);
+
+BITMAPINFO create_bitmap_info(int screenWidth, int screenHeight, int bitsPerPixel);
+
+std::optional<Frame> capture_frame(FrameContext& context);
+
+std::optional<Frame> extract_frame_from_bitmap(HDC screenDc, HBITMAP screenBitmap, int screenWidth, int screenHeight, int bitsPerPixel);
+
+
+
